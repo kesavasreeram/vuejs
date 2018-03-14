ef7aa6609468c878b0c2bbe41fa1d3567faf1831
Kesava Talabattula <kesavasreeram@gmail.com>
2018-03-14 12:10:13 -0500

    feat(eslint): add eslint configuration

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -4,6 +4,17 @@ const config = {
   entry: {
     app: path.resolve(__dirname, '../src/client-entry.js')
   },
+  // following rules in module property are used for eslint
+  module: {
+    rules: [
+      {
+        enforce: 'pre',  // makes sure that the source files are checked before loading for other loaders
+        test: /(\.js$)/, // File extensions you want to test
+        loader: 'eslint-loader', // loader
+        exclude: /node_modules/  // which modules are excluded
+      }
+    ]
+  },
   resolve: {
     alias: {
       vue: 'vue/dist/vue.js'
--- src/client-entry.js
+++ src/client-entry.js
@@ -1,12 +1,12 @@
 // import the app variable from app.js
-import { app } from './app';
+import { app } from './app'
 
 // mount the app variable to the DOM elements. The selection of dom elements is
 // done using the selection parameter, # is used for selecting elements by id
 // . is used for selecting elements by class. In case of class only first element of the class name
 // is selected
-app.$mount('#app');
+app.$mount('#app')
 
-if(module.hot) {
-  module.hot.accept();
+if (module.hot) {
+  module.hot.accept()
 }
