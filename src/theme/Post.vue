d245551097cf424ad620df84fac3fc417be58477
Kesava Talabattula <kesavasreeram@gmail.com>
2018-03-14 16:59:00 -0500

    feat(custom-properties): add custom properties to the components
    In most of the scenarios there will be a need to update data dynamically.
    This requires the need to pass the data dynamically, which can be acheived
    using the custom attributes to the components.

---
+++ src/theme/Post.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+    <div class="card-content">
+      <h3>{{ post.title }}</h3>
+      {{ post.content }}
+    </div>
+    <footer class="card-footer">
+      <!-- You can use v-bind:href or just :href to bind a dynamic value to the attribute-->
+      <a class="card-footer-item" v-bind:href="post.link" target="_blank">Read More</a>
+    </footer>
+  </div>
+</template>
+<script>
+  export default {
+    props: ['post'] // reserved property. THis is an array that bings the property of the component with the
+    // attribute of the element. In this case we used post.
+  }
+</script>
