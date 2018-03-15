1d9afc47a72f9690f766b2b3748a4aa09aefa57b
Kesava Talabattula <kesavasreeram@gmail.com>
2018-03-15 16:18:11 -0500

    feat(active-link): take levarage of vue-router inherent properties
    there are reserved properties within vue-router to apply styles to active routes
    using linkActiveClass and to automatically scoll the page to a specific y-location
    or last active location using scrollBehaviour

--- src/router.js
+++ src/router.js
@@ -7,7 +7,26 @@ import Login from './theme/Login.vue'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  mode: 'history', // this is needed or else it will add '#' in between domain and the page path
+  // this is needed or else it will add '#' in between domain and the page path.
+  // lets take advantage of the history api available within vue and skip the #
+  mode: 'history',
+  // if there is a tab and you want the active tab to be shown with updated styles
+  // linkActiveClass is a reserved property in vue-router to add a class to route which is active
+  linkActiveClass: 'is-active',
+  // you can also use
+  // scrollBehavior: (to, from, savedPosition) {
+  //   if (savedPosition) {
+  //     return savedPosition
+  //   }
+  //   if (to.hash) {
+  //     return { selector: to.hash }
+  //   }
+  // },
+  scrollBehavior: (to, from, savedPosition) => ({
+    y: 0
+  }),
+  // notice that both '/' and '/login' contain '/'. so is-active class is applied for both of the routes
+  // whenever /login is selected. this can be resolved using exact match.
   routes: [
     {
       path: '/',
--- src/theme/AppHeader.vue
+++ src/theme/AppHeader.vue
@@ -1,10 +1,25 @@
 <template>
   <nav class="navbar has-shadow">
     <div class="container">
-      <a href="/">
+      <!-- <a href="/">
         <img src="http://bit.ly/vue-img"
           alt="Vue SPA" />
-      </a>
+      </a> -->
+      <!-- using the anchor tag will make the whole page to reload when ever user clicks on the tag-->
+      <!-- we just want to make sure that the content is refreshed rather than the whole page reload-->
+      <!-- this can be achieved by using router-link tag. Router-link tells the application to update -->
+      <!-- the content of router-view with whatever content the router is providing -->
+      <router-link to="/">
+        <img src="http://bit.ly/vue-img"
+          alt="Vue SPA" />
+      </router-link>
+      <!-- lets add more route links to the page -->
+      <!-- 'exact' is a reserved attribute in vue to make sure that the router picks up exact route matching -->
+      <!-- for example consider /about and /about/contact . In this case if you havent used the 'exact' attribute -->
+      <!-- both /about and /about/contact are considered as active routes when /about/contact is selected and -->
+      <!-- displays the content of the route depending on which route is resolved at the end. Which is not desirable -->
+      <router-link class="navbar-item is-tab" to="/" exact>Home</router-link>
+      <router-link class="navbar-item is-tab" to="/login" exact>Login</router-link>
     </div>
   </nav>
 </template>
