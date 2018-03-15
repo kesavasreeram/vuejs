fa6aa066a16fcf0519e6675edf0db8341d05b57c
Kesava Talabattula <kesavasreeram@gmail.com>
2018-03-15 11:08:33 -0500

    feat(extract-styles): extract all styles into a separate file
    since all the styles are defined within the vue files, the styles are eventually
    stored within the compiles Javascript files. Which is not a best practice. So we
    use webpack extract plugin to extract all the styles into a separate css file.

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -1,6 +1,18 @@
 const base = require('./webpack.base.config')
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 const config = Object.assign({}, base, {
   plugins: base.plugins || []
 })
+
+// You can add the followig change within the vue-loader rule in base config file as well
+// only reason for doing this here is to make sure that base config file remains
+// generic and any custom changes go in here
+config.module.rules
+  .filter(x => { return x.loader == 'vue-loader' })
+  .forEach(x => x.options.extractCSS = true)
+
+config.plugins.push(
+  new ExtractTextWebpackPlugin('assets/styles.css')
+)
 
 module.exports = config
