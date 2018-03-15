cce33d897ef6f6f51274c3780e5f2c8e18351aa0
Kesava Talabattula <kesavasreeram@gmail.com>
2018-03-15 12:25:58 -0500

    feat(history-api): take advantage of history api
    in earlier HTML development, browsers will load the domain and then parse the path after '#'
    to load the content based on teh path and defined routes. History api will let us ignore the '#'

--- src/router.js
+++ src/router.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Category from './theme/Category.vue'
+import Login from './theme/Login.vue'
 
 // use the vue router within vue.
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routes: [{
-    path: '/',
-    component: Category
-  }]
+  mode: 'history', // this is needed or else it will add '#' in between domain and the page path
+  routes: [
+    {
+      path: '/',
+      component: Category
+    },
+    {
+      path: '/login',
+      component: Login
+    }
+  ]
 })
 
 export default router
