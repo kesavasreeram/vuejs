737a4965242f0cead40016a93e246480029f5cdc
Kesava Talabattula <kesavasreeram@gmail.com>
2018-03-15 10:41:17 -0500

    feat(scoped-styles): add custom styles to child components
    Make sure that the custom styles added in child components doesnt affect the styles
    of the whole application by using scoped styles.

--- src/theme/Post.vue
+++ src/theme/Post.vue
@@ -16,3 +16,24 @@
     // attribute of the element. In this case we used post.
   }
 </script>
+<style scoped>
+  /* scoped keyword within the the style tag makes sure that the styles defined within this vue component
+  doesnt bleed into the application. This makes sure that the application will not be affected adversely when
+  a style within a child component is changed.
+
+  In this example, we added custom styles to footer element within this child component. However if we didnt
+  scope these custom styles within this child component they will affect the parent footer element as well.
+  Which is not desired.*/
+  .card {
+    height: 100%;
+    padding-bottom: 40px;
+  }
+
+  footer {
+    bottom: 0;
+    left: 0;
+    position: absolute;
+    width: 100%;
+  }
+</style>
+
