83b321c9aa52bb57be2485139ada21f45a0ef4d7
Kesava Talabattula <kesavasreeram@gmail.com>
2018-03-16 15:49:21 -0500

    feat(JWT): add login module

--- src/app.service.js
+++ src/app.service.js
@@ -4,10 +4,28 @@ axios.defaults.baseURL = 'https://api.fullstackweekly.com'
 
 const appService = {
   getPosts (categoryId, pageSize) {
-    return new Promise((resolve) => {
-      axios.get(`/wp-json/wp/v2/posts?categories=${categoryId}&per_page=${pageSize}`)
+    return new Promise((resolve, reject) => {
+      axios
+        .get(
+          `/wp-json/wp/v2/posts?categories=${categoryId}&per_page=${pageSize}`
+        )
         .then(response => {
           resolve(response.data)
+        })
+        .catch(response => {
+          reject(response.status)
+        })
+    })
+  },
+  login (credentials) {
+    return new Promise((resolve, reject) => {
+      axios
+        .post('/service/auth.php', credentials)
+        .then(response => {
+          resolve(response.data)
+        })
+        .catch(response => {
+          reject(response.status)
         })
     })
   }
