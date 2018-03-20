e110a4c5893670221cfb42a18691576c5808ed1d
Kesava Talabattula <kesavasreeram@gmail.com>
2018-03-20 14:20:38 -0500

    feat(production-flags): add production flags to run code differently in production and development modes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path')
 const expressServer = express()
 const { createBundleRenderer } = require('vue-server-renderer')
 const serialize = require('serialize-javascript')
+const isProd = typeof process.env.NODE_ENV !== 'undefined' && (process.env.NODE_ENV === 'production')
 let renderer
 
 const indexHTML = (() => {
@@ -12,12 +13,19 @@ const indexHTML = (() => {
   return fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf-8')
 })()
 
-expressServer.use('/dist', express.static(path.resolve(__dirname, './dist')))
-
-require('./build/dev-server')(expressServer, bundle => {
-  // every time the server bundle changes, we will receive a bundle and generate a new renderer
-  renderer = createBundleRenderer(bundle)
-})
+// when running the production code, we do not need to hot reload the code
+// also we need to use dist folder as the root path
+if (isProd) {
+  expressServer.use('/', express.static(path.resolve(__dirname, './dist')))
+  const bundlePath = path.resolve(__dirname, './dist/server/main.js')
+  renderer = createBundleRenderer(fs.readFileSync(bundlePath, 'utf-8'))
+} else {
+  expressServer.use('/dist', express.static(path.resolve(__dirname, './dist')))
+  require('./build/dev-server')(expressServer, bundle => {
+    // every time the server bundle changes, we will receive a bundle and generate a new renderer
+    renderer = createBundleRenderer(bundle)
+  })
+}
 
 expressServer.get('*', (request, response) => {
   let context = { url: request.url }
